8c1de566000efcfb40d18096a0c63280409b8481
tiesen243 <ttien56906@gmail.com>
2024-09-05 04:43:15 +0700

    feat(actions): add detailed comments for actions
    Added detailed comments to the server actions to improve code
    readability and maintainability. This includes context definitions,
    initialization, middleware, and action descriptions for both public and
    protected actions.

--- server/actions/index.ts
+++ server/actions/index.ts
@@ -1,7 +1,23 @@
 import * as auth from '@/server/actions/routes/auth'
 import * as post from '@/server/actions/routes/post'
 
+/**
+ * Create a caller for the server actions.
+ * @example
+ * const res = await actions.post.getPosts()
+ *       ^? Post[]
+ */
 export const actions = {
   auth,
   post,
 }
+
+/**
+ * export type definition of API
+ * @example
+ * const posts: Awaited<ReturnType<Actions['post']['getPosts']>>
+ *      ^? data: {
+ *          posts: Post[]
+ *         }
+ */
+export type Actions = typeof actions
--- server/actions/safe-action.ts
+++ server/actions/safe-action.ts
@@ -5,32 +5,68 @@ import { z } from 'zod'
 import { auth } from '@/server/auth'
 import { db } from '@/server/db'
 
+/**
+ * 1. CONTEXT
+ *
+ * This section defines the "contexts" that are available in the backend API.
+ *
+ * These allow you to access things when processing a request, like the database, the session, etc.
+ *
+ * @see https://next-safe-action.dev/docs/define-actions/middleware#create-standalone-middleware
+ */
 const context = createMiddleware().define(async ({ next }) => {
   const { user, session } = await auth()
   return next({ ctx: { db, user, session } })
 })
 
+/**
+ * 2. INITIALIZATION
+ *
+ * This is where the safe-action client is initialized, connecting the context.
+ */
 const action = createSafeActionClient({
   validationAdapter: zodAdapter(),
   defaultValidationErrorsShape: 'flattened',
   defineMetadataSchema: () => z.object({ name: z.string().min(1, 'Action name is required') }),
 }).use(context)
 
+/**
+ * Middleware for timing action execution and adding an artificial delay in development.
+ *
+ * You can remove this if you don't like it, but it can help catch unwanted waterfalls by simulating
+ * network latency that would occur in production but not in local development.
+ */
 const timmingMiddleware = createMiddleware<{ metadata: { name: string } }>().define(
   async ({ next, metadata }) => {
-    const startTime = performance.now()
+    const start = performance.now()
+
     const result = await next()
-    const endTime = performance.now()
-    console.log(`Action "${metadata.name}" took ${endTime - startTime}ms to execute`)
+
+    const end = performance.now()
+
+    const time = Math.round((end - start) * 100) / 100
+    console.log(`[Action] ${metadata.name} took ${time}ms to execute`)
+
     return result
   },
 )
 
+/**
+ * Public (unauthenticated) action
+ *
+ * This action is available to anyone, regardless of whether they are logged in or not.
+ *  */
 export const publicAction = action.use(timmingMiddleware)
 
+/**
+ * Protected (authenticated) action
+ *
+ * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
+ * the session is valid and guarantees `ctx.session` and `ctx.user` is not null.
+ */
 export const protectedAction = action
   .use(timmingMiddleware)
   .use(async ({ next, ctx: { user, session } }) => {
-    if (!user || !session) throw new Error('You must be logged in to perform this action')
+    if (!session || !user) throw new Error('You must be logged in to perform this action')
     return next({ ctx: { user, session } })
   })
